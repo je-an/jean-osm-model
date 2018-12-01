ca26b7276cdb1cb4f054a7384181f930ac6f450c
je-an <mail@jeremiasandris.de>
2018-12-01 17:15:39 +0100

    Fix landuse heights

--- src/Building.js
+++ src/Building.js
@@ -4,21 +4,15 @@ define(["TypeCheck", "Failure"], function (TypeCheck, Failure) {
      * @constructor
      * @param {Object} options - options object
      * @param {String} options.id - the id of the dataset
-     * @param {}
+     * @param {String} options.name - the name of the dataset
+     * @param {Number[]} options.coordinates - all bulding coordinates [longitude, latitude, longitude, latitude, ...]
+     * @param {Number} options.levels - the building levels
      */
     var Building = function (options) {
         this.id = TypeCheck.isString(options.id) ? options.id : Failure.throwTypeError("options.id is not a string");
         this.name = TypeCheck.isString(options.name) ? options.name : Failure.throwTypeError("options.name is not a string");
         this.coordinates = TypeCheck.isArray(options.coordinates) ? options.coordinates : [];
         this.levels = TypeCheck.isNumber(options.levels) ? options.levels : 0;
-    };
-    /** @returns {Number[]} - the coordinates as an array */
-    Building.prototype.getCoordinatesArray = function () {
-        var a = [], i;
-        for (i = 0; i < this.coordinates.length; i++) {
-            a.push(parseFloat(this.coordinates[i].longitude), parseFloat(this.coordinates[i].latitude));
-        }
-        return Cesium.Cartesian3.fromDegreesArray(a);
-    };
+    };   
     return Building;
 });
--- src/Landuse.js
+++ src/Landuse.js
@@ -2,9 +2,11 @@ define(["TypeCheck", "Failure", "LanduseType"], function (TypeCheck, Failure, La
     /** 
      * @alias Landuse 
      * @constructor
-     * @param {Object} options - options object
+    * @param {Object} options - options object
      * @param {String} options.id - the id of the dataset
-     * @param {}
+     * @param {String} options.name - the name of the dataset
+     * @param {Number[]} options.coordinates - all bulding coordinates [longitude, latitude, longitude, latitude, ...]
+     * @param {LanduseType} options.type - the landuse type
      */
     var Landuse = function (options) {
         this.id = TypeCheck.isString(options.id) ? options.id : Failure.throwTypeError("options.id is not a string");
@@ -12,18 +14,10 @@ define(["TypeCheck", "Failure", "LanduseType"], function (TypeCheck, Failure, La
         this.coordinates = TypeCheck.isArray(options.coordinates) ? options.coordinates : [];
         this.type = TypeCheck.isEnumValue(options.type, LanduseType) ? options.type : Failure.throwTypeError("options.type is not a value of LanduseType");
     };
-    /** @returns {Number[]} - the coordinates as an array */
-    Landuse.prototype.getCoordinatesArray = function () {
-        var a = [], i;
-        for (i = 0; i < this.coordinates.length; i++) {
-            a.push(parseFloat(this.coordinates[i].longitude), parseFloat(this.coordinates[i].latitude));
-        }
-        return Cesium.Cartesian3.fromDegreesArray(a);
-    };
     /** @returns {Number[]} - the heights as an array */
     Landuse.prototype.getHeights = function () {
         var a = [], i;
-        for (i = 0; i < this.coordinates.length; i++) {
+        for (i = 0; i < this.coordinates.length / 2; i++) {
             a.push(3);
         }
         return a;
